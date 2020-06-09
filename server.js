69c484a7a83cf063beb92d3c009c58b78030a582
Elikworld <koladeodutoye9913@gmail.com>
2020-06-09 14:58:44 +0100

    first commit plus server setup

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/api/v1', (req, res) => {
+	res.send(` Welcome to the API Version 1.0.0 of SMS Notification App 
+        Please read the api documentation for how to go about its usage..
+    `);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log('Server is up and running');
+});
